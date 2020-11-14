48658ff36e87d2dc2811d2a8682e72b63bf46325
cedflam <cedflam@gmail.com>
2020-11-14 14:55:25 +0100

    add easyadmin with configuration

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Company;
+use App\Entity\Customer;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Bâtiment Buinoud');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+
+            yield MenuItem::section('Menu');
+            yield MenuItem::linkToCrud('Entreprise', 'fa fa-building', Company::class);
+            yield MenuItem::linkToCrud('Clients', 'fa fa-users', Customer::class);
+
+
+    }
+}
