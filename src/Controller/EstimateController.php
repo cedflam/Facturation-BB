c9f77a8a59a95c416d7e5864ade51e0bb5354e49
cedflam <cedflam@gmail.com>
2020-11-17 12:50:58 +0100

    finalize generation of estimate

--- src/Controller/EstimateController.php
+++ src/Controller/EstimateController.php
@@ -4,13 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Estimate;
 use App\Form\EstimateType;
+use App\Repository\CompanyRepository;
 use App\Repository\CustomerRepository;
 use App\Repository\EstimateRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class EstimateController extends AbstractController
 {
@@ -30,15 +38,22 @@ class EstimateController extends AbstractController
     private EstimateRepository $estimateRepository;
 
     /**
+     * @var CompanyRepository
+     */
+    private CompanyRepository $companyRepository;
+
+    /**
      * EstimateController constructor.
      * @param CustomerRepository $customerRepository
      * @param EntityManagerInterface $manager
      * @param EstimateRepository $estimateRepository
+     * @param CompanyRepository $companyRepository
      */
-    public function __construct(CustomerRepository $customerRepository, EntityManagerInterface $manager, EstimateRepository $estimateRepository)
+    public function __construct(CustomerRepository $customerRepository, EntityManagerInterface $manager, EstimateRepository $estimateRepository, CompanyRepository $companyRepository )
     {
         $this->customerRepository = $customerRepository;
         $this->estimateRepository = $estimateRepository;
+        $this->companyRepository = $companyRepository;
         $this->manager = $manager;
     }
 
@@ -47,7 +62,6 @@ class EstimateController extends AbstractController
      */
     public function listEstimates()
     {
-
         return $this->render('estimate/estimate_list.html.twig', [
             'estimates' => $this->estimateRepository->findAll()
         ]);
@@ -61,8 +75,31 @@ class EstimateController extends AbstractController
     public function showEstimate(Estimate $estimate)
     {
         return $this->render('estimate/estimate_show.html.twig', [
-            'estimate' => $this->estimateRepository->findOneBy(['id' => $estimate])
+            'estimate' => $this->estimateRepository->findOneBy(['id' => $estimate]),
+            'customer' => $this->customerRepository->findOneBy(['id' => $estimate->getCustomer()]),
+            'company' => $this->companyRepository->findOneBy(['id' => $estimate->getCustomer()->getCompany()])
         ]);
+    }
+
+    /**
+     * @Route("/devis/generer/pdf/{id}", name="estimate_generate_pdf")
+     *
+     * @param Environment $twig
+     * @param Pdf $pdf
+     * @param Estimate $estimate
+     * @return PdfResponse
+     */
+    public function generatePdf(Environment $twig, PDF $pdf, Estimate $estimate)
+    {
+        $customer = $this->customerRepository->findOneBy(['id'=> $estimate->getCustomer()]);
+
+        $html = $this->renderView('estimate/estimate_show.html.twig', [
+            'estimate' => $this->estimateRepository->findOneBy(['id' => $estimate]),
+            'customer' => $customer,
+            'company' => $this->companyRepository->findOneBy(['id' => $estimate->getCustomer()->getCompany()])
+        ]);
+
+        return new PdfResponse($pdf->getOutputFromHtml($html), 'devis-'.$customer->getFirstname().'-'.$customer->getLastname().'.pdf');
     }
 
     /**
