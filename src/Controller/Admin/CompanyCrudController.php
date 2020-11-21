f1eab8ae63debe5ae0285c1b7e34a2e5ea8f26de
cedflam <cedflam@gmail.com>
2020-11-21 10:18:42 +0100

    setting up admin dashboard

--- src/Controller/Admin/CompanyCrudController.php
+++ src/Controller/Admin/CompanyCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Company;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -16,13 +17,16 @@ class CompanyCrudController extends AbstractCrudController
     }
 
 
-    /*public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title', 'Titre'),
-            TextEditorField::new('description', 'Descripiton'),
-        ];
-    }*/
+  public function configureCrud(Crud $crud): Crud
+  {
+      return $crud
+          ->setEntityLabelInSingular('Entreprise')
+          ->setEntityLabelInPlural('Entreprises')
+          ->setPageTitle('index', 'Liste des %entity_label_plural%')
+
+          ;
+  }
+
+
 
 }
--- src/Controller/Admin/CustomerCrudController.php
+++ src/Controller/Admin/CustomerCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Customer;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CustomerCrudController extends AbstractCrudController
 {
@@ -12,14 +14,27 @@ class CustomerCrudController extends AbstractCrudController
         return Customer::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Client')
+            ->setEntityLabelInPlural('Clients')
+            ->setPageTitle('index', 'Liste des %entity_label_plural%')
+
+            ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstname', 'Prénom'),
+            TextField::new('lastname', 'Nom'),
+            TextField::new('address', 'Adresse'),
+            TextField::new('postCode', 'Code Postal'),
+            TextField::new('city', 'Ville'),
+            TextField::new('email', 'Email'),
+            TextField::new('phone', 'Téléphone'),
+
         ];
     }
-    */
 }
