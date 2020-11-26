289ea1318d6a27e5a3e4045e8a56ae34440df928
cedflam <cedflam@gmail.com>
2020-11-26 11:38:31 +0100

    fix bug calcul advance invoice

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -12,7 +12,6 @@ jQuery(document).ready(function () {
         div.append(tmpl);
         // J'initialise les functions
         handleDeleteButton();
-        calculFields();
 
         $('.calcul').removeClass('hidden');
     });
@@ -25,32 +24,41 @@ jQuery(document).ready(function () {
             $('.calcul').removeClass('hidden');
             let id = $(this).data('target');
             $('#' + id).remove();
+
         });
     }
 
-    // Calcul des champs
-    function calculFields() {
-        //J'effectue le calcul
-        $('.calcul').on('click', function () {
+    //J'effectue le calcul
+    $('.calcul').on('click', function () {
 
-            let totalTTC = $('.totalTTC').val();
-            let restantDu = $('.remainingCapital').val();
+        // J'initialise les valeurs
+        // Fixe un bug lors du calcul après la suppression du dernier acompte
+        $('.totalAdvance').val(0);
+        $('.remainingCapital').val($('.totalTTC').val());
 
-            // Je boucle sur les montants
-            let amounts = $('.amount');
-            let totalAmount = 0;
-            amounts.each(function () {
-                totalAmount = totalAmount + (+$(this).val());
-                restantDu = totalTTC - totalAmount;
-                console.log(totalAmount)
-                $('.remainingCapital').val(restantDu.toFixed(2));
-                $('.totalAdvance').val(totalAmount.toFixed(2));
-            });
+        // Je stocke les valeurs dans des variables
+        let totalTTC = $('.totalTTC').val();
+        let restantDu = $('.remainingCapital').val();
 
-            // Je cache le bouton une fois le calcul effectué
-            $('.calcul').addClass('hidden');
+        // Je boucle sur les montants
+        let amounts = $('.amount');
+        let totalAmount = 0;
+        amounts.each(function () {
+
+            totalAmount = totalAmount + (+$(this).val());
+            restantDu = totalTTC - totalAmount;
+
+            $('.remainingCapital').val(restantDu.toFixed(2));
+            $('.totalAdvance').val(totalAmount.toFixed(2));
+
+            console.log(amounts.length);
         });
-    }
+
+        // Je cache le bouton une fois le calcul effectué
+        $('.calcul').addClass('hidden');
+    });
+
+
     handleDeleteButton();
-    calculFields();
+
 });
