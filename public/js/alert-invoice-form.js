dffdc4637c8bf856a2199d020cf1c9b8e6e9468e
cedflam <cedflam@gmail.com>
2020-12-08 00:31:44 +0100

    add alert in invoice form

--- public/js/alert-invoice-form.js
+++ public/js/alert-invoice-form.js
@@ -1,15 +1,15 @@
 $('document').ready(function () {
     // Affiche des alertes au clic sur les champs en bas du formulaire de calcul
     $('.total-advances').on('click', function () {
-        alert('Vous ne ddevez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
+        alert('Vous ne devez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
     });
     $('.total-ht').on('click', function () {
-        alert('Vous ne ddevez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
+        alert('Vous ne devez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
     });
     $('.total-ttc').on('click', function () {
-        alert('Vous ne ddevez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
+        alert('Vous ne devez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
     });
     $('.remaining-capital').on('click', function () {
-        alert('Vous ne ddevez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
+        alert('Vous ne devez pas modifier ces champs manuellement ! Vous devez ajouter un acompte et utiliser le bouton "Calculer" ')
     });
 });
