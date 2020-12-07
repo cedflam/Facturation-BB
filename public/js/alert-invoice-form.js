e838338d8c0a48ada1a891fd325fe56332f89866
cedflam <cedflam@gmail.com>
2020-12-08 00:18:03 +0100

    add alert script in invoice form

---
+++ public/js/alert-invoice-form.js
@@ -0,0 +1,15 @@
+$('document').ready(function () {
+    $('.total-advances').on('click', function () {
+        alert('Vous ne devez pas modifier ce champ manuellement ! Ajoutez un acompte puis cliquez sur le bouton calculer !')
+    });
+    $('.total-ht').on('click', function () {
+        alert('Vous ne devez pas modifier ce champ manuellement ! Ajoutez un acompte puis cliquez sur le bouton calculer !')
+    });
+    $('.total-ttc').on('click', function () {
+        alert('Vous ne devez pas modifier ce champ manuellement ! Ajoutez un acompte puis cliquez sur le bouton calculer !')
+    });
+    $('.remaining-capital').on('click', function () {
+        alert('Vous ne devez pas modifier ce champ manuellement ! Ajoutez un acompte puis cliquez sur le bouton calculer !')
+    });
+
+});
